3fda94c8dc2c132273c97ecbaed268c65f537497
ejb1123 <ej@ejb1123.tk>
2014-12-16 15:42:09 -0500

    token connection setup now working

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-var socket1 = new WebSocket("ws://127.0.0.1:25564/");
-socket1.onmessage= function(event){
-    console.log(event.data);
+function createSocket(url0,token){
+    var socket1 = new WebSocket(url0);
+    socket1.onmessage= function(event){
+        console.log(event.data);
+        switch(event.data) {
+            case "rx:ok":
+                $('#tokenModal').modal('hide');
+                break;
+            default:
+                break;
+        }
+    }
+    socket1.onopen = function(event){
+        socket1.send(token);
+    }
 }
-socket1.onopen = function(event){
-    socket1.send("token:UNDEFINED");
-    socket1.send("token:UNDEFINED");
-}
+//var socket1 = new WebSocket("ws://127.0.0.1:25564/");
+
+
 function send(hh){
     socket1.send(hh);
 }
